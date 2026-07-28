b20af37cb8c7e2ad1bb26b383bcf1acd5d8460c3
Mangat Rai <mangatrai@gmail.com>
2026-07-28 16:08:48 -0500

    Add branded social-card image, replacing stock Docusaurus asset
    Closes #10.

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -101,8 +101,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/academy-social-card.png',
     metadata: [
       {
         name: 'description',
